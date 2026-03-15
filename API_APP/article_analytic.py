42670dfea8eca4c6a91e2c5e84e6b76285422a66
user <samoschevav2008@gmail.com>
2026-03-15 19:49:46 +0300

    Добавлен обновленный article_analytic.py в API_APP

--- API_APP/article_analytic.py
+++ API_APP/article_analytic.py
@@ -1,18 +1,16 @@
 import requests as r
+
 url = r.get('https://newsapi.org/v2/everything?apiKey=bab0f95a220a4043b5a07b40b88b1b11&q=servals&pageSize=50&sortBy=publishedAt').json()
 
-result = [
-    {
-    "title": a["title"],
-    "source": a["source"]["name"],
-    "published": a["publishedAt"],
-    "author": a["author"]
-    }
-    for a in url["articles"]
-    if a["title"] and a["title"].strip()
-    and a["url"] and a["url"].startswith("http")
-    and a["description"] and len(a["description"]) >= 50
-    ]
+result = []
+for a in url["articles"]:
+    if a["title"] and a["url"] and a["description"] and len(a["description"]) >= 50:
+        result.append({
+            "title": a["title"],
+            "source": a["source"]["name"],
+            "published": a["publishedAt"],
+            "author": a["author"]
+        })
 
 print(result)
 print(f"Количество статей: {len(result)}")
