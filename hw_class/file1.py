6c16de4994951b6005fac4e503ec92080fc1b697
user <samoschevav2008@gmail.com>
2026-03-30 00:30:10 +0300

    ok

---
+++ hw_class/file1.py
@@ -0,0 +1,56 @@
+#1-3
+class Facade:
+    pass
+
+facade_1 = Facade()
+facade_1_type = type(facade_1)
+print(facade_1_type)
+
+#4
+class Grade:
+    minimum_passing = 65
+
+#5
+class Rules:
+    def washing_brushes(self):
+        return "Point bristles towards the basin while washing your brushes."
+
+#6-n
+class Circle:
+    pi = 3.14
+
+    def __init__(self, diameter):
+        self.radius = diameter // 2
+
+    def area(self, radius):
+        return self.pi * radius ** 2
+
+    def circumference(self):
+        return 2 * self.pi * self.radius
+
+    def __repr__(self):
+        return f"Circle with radius {self.radius}"
+
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza.circumference())
+print(teaching_table.circumference())
+print(round_room.circumference())
+
+print(medium_pizza)
+print(teaching_table)
+print(round_room)
+
+#n-1
+print(dir(5))
+
+def this_function_is_an_object():
+    pass
+print(dir(this_function_is_an_object()))
+
+
+
+
+
